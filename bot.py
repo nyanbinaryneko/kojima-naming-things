22d16aa24cf0a57ad25341676e3c19bd6f315355
Ali <3050609+alikoneko@users.noreply.github.com>
2019-11-10 16:23:58 -0500

    added some fun things

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
 import tweepy
 import os
-from name_generator import generate_name
+from name_generator import generate_tweet
 import random 
 
 DEBUG = os.environ["DEBUG"].lower() == "true"
-name = generate_name() 
-print(f"kojima name: {name}")
 print(f"DEBUG={DEBUG}")
-sentences = [f"Thinking my next game will have {name} as the main character.", 
-    f"hmmm, is {name} a good name? I'm thinking it fits the character and my vision", 
-    f"Ah, yes, {name}. They will be played by Norman Reedus.",
-    f"{name} will be an otter furry played by Conan O'brien."]
-sentence = random.choice(sentences)
-print(sentence)
+tweet = generate_tweet()
+print(tweet)
 if not DEBUG:
     CONSUMER_API = os.environ["CONSUMER_API_KEY"]
     CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
@@ -21,4 +15,4 @@ if not DEBUG:
     auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
-    api.update_status(f"{sentence}")
+    api.update_status(f"{tweet}")
--- name_generator.py
+++ name_generator.py
@@ -1,12 +1,35 @@
 import random
 
 def generate_name():
-    with open("./corpus/nouns.txt") as f:
-        nouns = f.readlines()
-    nouns = [noun.strip() for noun in nouns]
+    nouns = open_file("./corpus/nouns.txt")
+    adjectives = open_file("./corpus/adjectives.txt")
+    names = open_file("./corpus/names.txt")
+    animals = open_file("./corpus/animals.txt")
+    names += adjectives + animals
+    if random.randint(0, 5) < 2:
+        name = metal_gear_solid_name_generator(animals, adjectives)
+    else:
+        name = f"{random.choice(names).capitalize()} {random.choice(nouns).capitalize()} {random.choice(nouns).capitalize()}man"
+    return name
 
-    with open("./corpus/adjectives.txt") as f:
-        adjectives = f.readlines()
-    adjectives = [adjective.strip() for adjective in adjectives]
+def generate_tweet():
+    friends = open_file("./corpus/friends.txt")
+    animals = open_file("./corpus/animals.txt")
+    name = generate_name()
+    sentences = [f"Thinking my next game will have {name} as the main character.", 
+    f"hmmm, is {name} a good name? I'm thinking it fits the character and my vision", 
+    f"Ah, yes, {name}. They will be played by {random.choice(friends)}.",
+    f"{name} will be an {random.choice(animals)} furry played by {random.choice(friends)}.",
+    f"Konami wouldn't let me have a character named {name}. They will be in my next game, I promise."]
 
-    return f"{random.choice(adjectives).capitalize()} {random.choice(nouns).capitalize()} {random.choice(nouns).capitalize()}man"   
+    return random.choice(sentences)
+
+def open_file(file):
+    with open(file) as f:
+        lyst = f.readlines()
+    lyst = [item.strip() for item in lyst]
+    return lyst
+
+def metal_gear_solid_name_generator(animals, adjectives):
+    print("making an mgs name")
+    return f"{random.choice(adjectives).capitalize()} {random.choice(adjectives).capitalize()}"
