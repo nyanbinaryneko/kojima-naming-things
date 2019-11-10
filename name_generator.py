190a9ff0b953a325839689ea5a63384396c7ce43
Ali <3050609+alikoneko@users.noreply.github.com>
2019-11-10 17:00:15 -0500

    more sayings

--- name_generator.py
+++ name_generator.py
@@ -20,9 +20,18 @@ def generate_tweet():
     f"hmmm, is {name} a good name? I'm thinking it fits the character and my vision", 
     f"Ah, yes, {name}. They will be played by {random.choice(friends)}.",
     f"{name} will be an {random.choice(animals)} furry played by {random.choice(friends)}.",
-    f"Konami wouldn't let me have a character named {name}. They will be in my next game, I promise."]
+    f"Konami wouldn't let me have a character named {name}. They will be in my next game, I promise.",
+    f"I'm trying find a role for {random.choice(friends)} do you think {name} would be a good role for them?",
+    f"📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍📦 🐍",
+    f"{name} will be the weapon to surpass Metal Gear.",
+    f"I want to work with {random.choice(friends)} again. Think they'd like to be {name} in my next game?",
+    f"I am 70% movies."]
 
-    return random.choice(sentences)
+    sentence = random.choice(sentences)
+    if "📦 🐍" in sentence:
+        print("BOX MODE!!!!!!!!!!!!!!!")
+
+    return sentence
 
 def open_file(file):
     with open(file) as f:
